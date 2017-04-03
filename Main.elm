bcd9240ad14940cd77153483307980e3ceddbe0c
xarvh <francesco.orsenigo@gmail.com>
2017-04-03 10:59:06 +1000

    wip

--- Main.elm
+++ Main.elm
@@ -24,12 +24,11 @@ main =
             """
             ## Namespacing
 
-            > What if two different parts of our CSS declaration use the same class name?
+            _What if two different parts of our CSS declaration use the same class name?_
 
-            -> I want to pick class names without fear of collision
-            -> I want self-contained modules
+            * I want to pick class names without fear of collision
+            * I want self-contained modules
             """
-
         , md
             """
             ## Expressive Reuse
@@ -38,26 +37,29 @@ main =
             warningColor =
               "#ccdd33"
 
-            flashBackgroundColor =
-              addFlashBackgroundOpacity(warningColor)
+            warningPopupStyle =
+              makePopupStyle(warningColor)
             ```
 
-            -> I want full programming power: variables, modules, functions, lists...
+            * I want full programming power: variables, modules, functions, lists...
             """
-
         , md
             """
             ## Inlining
 
-            ```
+            ```elm
             helloWorld = "Hello World"
-
-            printHelloWorld =
-              console.log(helloWorld)
             ```
-            > Sometimes I want a :hover effect without having to name a class
 
-            -> I want to declare styles in place, with full pseudo-class support
+            ```elm
+            printHelloWorld =
+              console.log(SomeImportedModule.helloWorld)
+            ```
+            _Sometimes I want a `:hover` effect without having to name a class_
+
+            * I want to declare styles in place
+            * ...with full pseudo-class support
+            * ...and without cluttering the browser's dev tools
             """
         ]
 
